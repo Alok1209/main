bfa5c7df268c4caecdde63a2d150aa935827bbd2
Alok Pratap Verma <alok12verma903@gmail.com>
2023-11-11 19:59:11 +0530

    i18n addition in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-// import React from 'react';
+import React from 'react';
 import  ReactDOM from "react-dom";
 // // import "./index.css"
   // import App from './App'
   import Project from "./Project";
+  import './i18n.js'
 ReactDOM.render(<Project/>,document.getElementById('root'));
